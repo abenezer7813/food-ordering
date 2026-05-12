9786207045e892bb43cd7f2327363bdd97710389
abenezer7813 <abenimom1994@gmail.com>
2026-05-13 00:21:57 +0300

    fix

--- backend/src/utils/email.ts
+++ backend/src/utils/email.ts
@@ -1,10 +1,13 @@
 import nodemailer from 'nodemailer'
 import 'dotenv/config'
+import dns from 'dns'
+dns.setDefaultResultOrder('ipv4first')
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
   secure: false,
+  
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
