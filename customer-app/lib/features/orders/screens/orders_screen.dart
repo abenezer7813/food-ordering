a0e9cccd88745587672916b0a8e92d3bbdb6997b
Codesofnati <nathnaelasefa80@gmail.com>
2026-05-25 12:44:11 +0300

    orders screen modified

--- customer-app/lib/features/orders/screens/orders_screen.dart
+++ customer-app/lib/features/orders/screens/orders_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import '../../../core/constants/app_colors.dart';
 import '../providers/order_provider.dart';
-
+import '../../../shared/widgets/app_drawer.dart';
 class OrdersScreen extends ConsumerWidget {
   const OrdersScreen({super.key});
 
@@ -14,12 +14,17 @@ class OrdersScreen extends ConsumerWidget {
     return Scaffold(
       backgroundColor: AppColors.mainBg,
       appBar: AppBar(
+        iconTheme: const IconThemeData(
+    color: AppColors.appBarNav2,
+  ),
         backgroundColor: AppColors.accent,
         title: const Text(
           'My Orders',
-          style: TextStyle(color: AppColors.textLight),
+          style: TextStyle(color: AppColors.textLight, fontWeight: FontWeight.bold),
         ),
       ),
+            drawer: const AppDrawer(),
+
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: 1,
         selectedItemColor: AppColors.accent,
@@ -27,7 +32,7 @@ class OrdersScreen extends ConsumerWidget {
         onTap: (index) {
           if (index == 0) context.go('/lounges');
           if (index == 1) context.go('/orders');
-          if (index == 2) context.push('/history');
+          if (index == 2) context.go('/history');
         },
         items: const [
           BottomNavigationBarItem(
